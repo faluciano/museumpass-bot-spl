8c04621157444b9032cf265509d131365fd1ed27
Felix Luciano Salomon <felixluciano.a@gmail.com>
2024-08-20 15:27:51 -0700

    Added daily email timer

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿class Program
+﻿using System.Threading.Tasks;
+class Program
 {
+    static Timer? _timer;
     static async Task Main()
+    {
+        // Run a web server in a separate task
+
+        // Run email service logic
+        _timer = new Timer(async _ => await RunEmailService(), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+
+        // Wait for the web server to complete
+
+    }
+
+    static async Task RunEmailService()
     {
         DotNetEnv.Env.Load();
         var locations = await MuseumRequest.ParseMuseums("30");
 
         await EmailService.EmailService.SendEmail(locations);
-
     }
 
     static void Print_results(Dictionary<string, Tuple<string, List<string>>> values)
